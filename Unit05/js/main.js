9f4ef41f78542ae2b21ba5756a31b36cd2f55c2a
Packer Suxo-Sanchez <Packer@Packers-Mac-mini.local>
2020-10-25 22:05:07 -0600

    Finished JavaScript sorting and added CSS

--- Unit05/js/main.js
+++ Unit05/js/main.js
@@ -1,5 +1,5 @@
 // Create an array with 4 trees listed
-const trees = ['oak', 'Pine', 'aspn', 'Bald Cypress']
+const trees = ['oak', 'Pine', 'aspen', 'Bald Cypress']
 const errorElement = document.querySelector('#error')
 const displayResults = document.querySelector('#displayResults')
 
@@ -29,6 +29,57 @@ document.querySelector('#removeTree1').onclick = () => {
         trees.shift()
         listTrees()
     } else {
-        errorElement.textContent = 'Cannot remove first tree because there are no trees!'
+        errorElement.textContent = 'Cannot remove first tree because there are no more trees!'
+    }
+}
+
+document.querySelector('#removeTree2').onclick = () => {
+    if (trees.length > 1) {
+        trees.splice(1, 1)
+        listTrees()
+    } else {
+        errorElement.textContent = 'There is no second tree to remove!'
+    }
+    
+}
+
+document.querySelector('#removeTreeLast').onclick = () => {
+    if (trees.length > 0) {
+        trees.pop()
+        listTrees()
+    } else {
+        errorElement.textContent = 'Cannot remove last tree because there are no more trees!'
+    }
+}
+
+document.querySelector('#sortTrees').onclick = () => {
+    trees.sort((a,b) => {
+        return a.toLowerCase().localeCompare(b.toLowerCase())
+    })
+    listTrees()
+}
+
+document.querySelector('#lowerTrees').onclick = () => {
+    trees.forEach((tree, index, array) => {
+        array[index] = tree.toLowerCase()
+    })
+    listTrees()
+}
+
+document.querySelector('#showName3').onclick = () => {
+    if (trees.length > 2) {
+        const treeThree = trees[2]
+        errorElement.textContent = `The third tree is a(n) ${treeThree} tree!`
+    } else {
+        error.textContent = 'Sorry, there are less than three trees!'
+    }
+}
+
+document.querySelector('#showName4').onclick = () => {
+    if (trees.length > 3) {
+        const treeFour = trees[3]
+        errorElement.textContent = `The fourth tree is a(n) ${treeFour} tree!`
+    } else {
+        errorElement.textContent = 'Sorry, there are less than four trees!'
     }
 }
