7e29463a1937bf217480489282145f523698f3ac
Packer Suxo-Sanchez <Packer@Packers-Mac-mini.local>
2020-11-24 13:45:46 -0700

    Edited story and added more CSS

--- Unit06/js/main.js
+++ Unit06/js/main.js
@@ -15,7 +15,7 @@ function tellStory() {
         return verb.toLowerCase();
     })
 
-    const myStory = `Once upon a time there were four <span>${adj[0]} ${noun[0]}s</span> named Flopsy, Mopsy, Cotton-tail, and Peter. They lived with their mother underneath a <span>${noun[1]}</span> near a <span>${adj[1]} ${noun[2]}</span>. Now, my dears, said <span>${adj[0]}</span> Mrs. Rabbit, you may go over the <span>${noun[3]}</span> or around the <span>${noun[4]}</span>, but do not go into Mr. McGregor\'s <span>${noun[5]}</span>, your father had an accident there and he was put in a <span>${noun[6]}</span> by Mrs. McGregor. Now <span>${verb[0]}</span> along, and don't get into mischief. I am going <span>${verb[1]}ing</span>.`
+    const myStory = `Once upon a time there was a <span>${adj[0]} ${noun[0]}</span> named Bo. He lived with his mother underneath a <span>${noun[1]}</span> near a <span>${adj[1]} ${noun[2]}</span>. One day she said to <span>${adj[0]}</span> Bo: You may go over the <span>${noun[3]}</span> or around the <span>${noun[4]}</span>, but do not go into the neighbor\'s <span>${noun[5]}</span>; your father had an accident there and he was put in a <span>${noun[6]}</span> by the neighbor. Now <span>${verb[0]}</span> along, and don't get into mischief. I need to go <span>${verb[1]}ing</span>.`
 
     document.querySelector('#story').innerHTML = myStory
 }
