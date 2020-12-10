6cfe6e0a0eab3cafead984caf3ad0e65ae420926
Packer Suxo-Sanchez <Packer@Packers-Mac-mini.local>
2020-12-09 19:47:25 -0700

    Added click events to links with a for each loop

--- Unit10/js/script.js
+++ Unit10/js/script.js
@@ -10,3 +10,28 @@ async function getHotelData() {
 let hotelData = {}
 
 getHotelData().then(data => hotelData = data)
+
+// document.querySelectorAll('a').addEventListener('click',hotelInfo)
+// store in a variable  ^^^
+// use that variable to loop over each element and addEventListener to each one
+// use forEach
+let hotelLinks = document.querySelectorAll('a').forEach((hotelLink) => {
+    hotelLink.addEventListener('click', hotelInfo)
+})
+
+// document.querySelector('#marriott').addEventListener('click', hotelInfo)
+// document.querySelector('#sheraton').addEventListener('click', hotelInfo)
+// document.querySelector('#hilton').addEventListener('click', hotelInfo)
+
+function hotelInfo(event) {
+    let hotelChoice = hotelData.hotels.find(hotel => {
+        return event.target.id === hotel.name.toLowerCase()
+    })
+    console.log(hotelChoice)
+    document.querySelector('#hotelName').textContent = `${hotelChoice.name} Hotel`
+    document.querySelector('#address').textContent = `${hotelChoice.address}`
+    document.querySelector('#rooms').textContent = `${hotelChoice.rooms}`
+    document.querySelector('#gym').textContent = `${hotelChoice.gym}`
+    document.querySelector('#type').textContent = `${hotelChoice.roomTypes}`
+    document.querySelector('#picture').setAttribute('src', hotelChoice.picture)
+}
