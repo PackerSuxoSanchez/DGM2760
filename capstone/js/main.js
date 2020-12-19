0958ff1aee506c98b4634c9eb4c703f836c82914
Packer Suxo-Sanchez <packer@suxosanchez.net>
2020-12-18 17:52:14 -0700

    Switched from "Employee" to "Agent"

--- capstone/js/main.js
+++ capstone/js/main.js
@@ -1,8 +1,28 @@
-async function getAgentInfo() {
+async function getAgentData() {
     try {
         const response = await fetch('agents.json')
         return await response.json()
     } catch (error) {
         console.error(error)
     }
+}
+
+let agentData = {}
+
+getAgentData().then(data => agentData = data)
+
+let agentLinks = document.querySelectorAll('a').forEach((agentLink) => {
+    agentLink.addEventListener('click', agentInfo)
+})
+
+function agentInfo(event) {
+    let agentChoice = agentData.agents.find(agent => {
+        return event.target.id === agent.name.toLowerCase()
+    })
+    console.log(agentChoice)
+    document.querySelector('#agentName').textContent = `${agentChoice.name}`
+    document.querySelector('#title').textContent = `${agentChoice.title}`
+    document.querySelector('#schedule').textContent = `${agentChoice.schedule}`
+    document.querySelector('#experience').textContent = `${agentChoice.experience}`
+    document.querySelector('#focus').textContent = `${agentChoice.focus}`
 }
